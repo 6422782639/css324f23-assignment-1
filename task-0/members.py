1626e082e093dd38f388cbd742959ddef4f81247
6422782639 <144681321+6422782639@users.noreply.github.com>
2023-09-11 23:13:46 +0700

    Update members.py

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Arthima Khroungchan", "sid": "6422782639"},
+    {"name": "Warit Khuntontong", "sid": "6422770907"},
 ]
